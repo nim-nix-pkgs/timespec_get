010d5f8fb02bfca91adc1df26fee61c64838faca
hugosenari <hugosenari@gmail.com>
2022-03-12 03:37:50 -0300

    chore: re index timespec_get

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nanosecond-percision time using timespec_get'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."timespec_get-master".url = "path:./master";
+  inputs."timespec_get-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."timespec_get-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
